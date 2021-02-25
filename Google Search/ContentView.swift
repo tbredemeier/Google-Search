e218dded7d34417a51dd32f5733d1eccedda4c36
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-25 08:01:27 -0600

    Initial Commit

---
+++ Google Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Google Search
+//
+//  Created by Tom Bredemeier on 2/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Google Search/Google_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_SearchApp.swift
+//  Google Search
+//
+//  Created by Tom Bredemeier on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Google_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
